84297f2b375da9708f1db0d44e92b77311dd379f
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2021-03-08 09:48:07 +0200

    Added "First steps in coding exercise" home work

---
+++ 01.CSharp-Basics/02.FirstStepsInCodingExercise/RadiansToDegrees/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 01.CSharp-Basics/02.FirstStepsInCodingExercise/USD2BGN/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USD2BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
