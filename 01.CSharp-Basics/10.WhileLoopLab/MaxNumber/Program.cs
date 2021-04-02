b165ce2db01aa615b762bf45f88e0d6e0b841080
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2021-04-02 16:39:40 +0300

    j

---
+++ 01.CSharp-Basics/10.WhileLoopLab/MaxNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = Int32.MinValue;
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                int number = int.Parse(input);
+                if (number > max)
+                {
+                    max = number;
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ 01.CSharp-Basics/10.WhileLoopLab/Sequence2kPlus1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sequence2kPlus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int n = 1;
+            while (number >= n)
+            {
+                Console.WriteLine(n);
+                n = n * 2 + 1;
+            }
+        }
+    }
+}
