2bfb09c793679adcffc25c1a1b6bc8e4535e51c5
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2021-04-06 14:19:31 +0300

    Upload "CSharp Nested Loops - Exercise" homework

---
+++ 01.CSharp-Basics/14.NestedLoopsExercise/EqualSumsEvenOddPosition/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace EqualSumsEvenOddPosition
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            for (int i = first; i <= second; i++)
+            {
+                string number = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for (int j = 1; j <= number.Length; j++)
+                {
+                    int n = int.Parse(number[j - 1].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += n;
+                    }
+                    else
+                    {
+                        oddSum += n;
+                    }
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
---
+++ 01.CSharp-Basics/14.NestedLoopsExercise/SumPrimeNonPrime/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SumPrimeNonPrime
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = string.Empty;
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else if (number > 0)
+                {
+                    bool isPrime = true;
+                    for (int i = 2; i < number; i++)
+                    {
+                        if (number % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                    {
+                        primeSum += number;
+                    }
+                    else
+                    {
+                        nonPrimeSum += number;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
