0647c4b0de479fdb23d8c13a6541a7f7647030dd
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2021-07-09 16:39:34 +0300

    Last task from "Object and classes labs" homework

--- 02.Programming-Fundamentals-With-CSharp/06.ObjectsAndClasses-Lab/ObjectsAndClassesLab/VehicleCatalogue/Catalogue.cs
+++ 02.Programming-Fundamentals-With-CSharp/06.ObjectsAndClasses-Lab/ObjectsAndClassesLab/VehicleCatalogue/Catalogue.cs
@@ -1,12 +1,98 @@
-﻿using System;
-
-namespace VehicleCatalogue
+﻿namespace VehicleCatalogue
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Truck
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Weight { get; set; }
+    }
+
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+    }
+
+    public class Catalog
+    {
+        public ICollection<Truck> Trucks;
+        public ICollection<Car> Cars;
+
+        public Catalog()
+        {
+            this.Trucks = new List<Truck>();
+            this.Cars = new List<Car>();
+        }
+    }
+
+    public class Catalogue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var catalog = PopulateVehicleCatalog();
+            PrintVehicleCatalog(catalog);
+        }
+
+        private static Catalog PopulateVehicleCatalog()
+        {
+            var catalog = new Catalog();
+            var input = Console.ReadLine() ?? throw new ArgumentNullException();
+            while (input != "end")
+            {
+                var data = input.Split("/", StringSplitOptions.RemoveEmptyEntries);
+                var type = data[0];
+                var brand = data[1];
+                var model = data[2];
+                var parameter = int.Parse(data[3]);
+                if (type.ToLower() == "truck")
+                {
+                    catalog.Trucks.Add(new Truck()
+                    {
+                        Brand = brand,
+                        Model = model,
+                        Weight = parameter,
+                    });
+                }
+                else
+                {
+                    catalog.Cars.Add(new Car()
+                    {
+                        Brand = brand,
+                        Model = model,
+                        HorsePower = parameter,
+                    });
+                }
+
+                input = Console.ReadLine() ?? throw new ArgumentNullException();
+            }
+
+            return catalog;
+        }
+
+        private static void PrintVehicleCatalog(Catalog catalog)
+        {
+            if (catalog.Cars.Any())
+            {
+                Console.WriteLine($"Cars:");
+                foreach (var car in catalog.Cars.OrderBy(c => c.Brand))
+                {
+                    Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+                }
+            }
+
+            if (catalog.Trucks.Any())
+            {
+                Console.WriteLine("Trucks:");
+                foreach (var truck in catalog.Trucks.OrderBy(t => t.Brand))
+                {
+                    Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+                }
+            }
         }
     }
 }
