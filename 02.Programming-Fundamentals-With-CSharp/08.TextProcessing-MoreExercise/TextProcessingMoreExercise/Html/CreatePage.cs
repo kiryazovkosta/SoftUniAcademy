37172a678aedeab7881e7689f3719ffd0af1653f
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2021-07-27 15:30:31 +0300

    Upload "Text Processing - More Exercise" homework

---
+++ 02.Programming-Fundamentals-With-CSharp/08.TextProcessing-MoreExercise/TextProcessingMoreExercise/Html/CreatePage.cs
@@ -0,0 +1,33 @@
+﻿namespace Html
+{
+    using System;
+    using System.Text;
+    public class CreatePage
+    {
+        static void Main(string[] args)
+        {
+            string tab = "    ";
+            StringBuilder html = new StringBuilder();
+            string title = Console.ReadLine();
+            html.AppendLine("<h1>");
+            html.AppendLine($"{tab}{title}");
+            html.AppendLine("</h1>");
+
+            string content = Console.ReadLine();
+            html.AppendLine("<article>");
+            html.AppendLine($"{tab}{content}");
+            html.AppendLine("</article>");
+
+            string input = Console.ReadLine();
+            while (input != "end of comments")
+            {
+                html.AppendLine("<div>");
+                html.AppendLine($"{tab}{input}");
+                html.AppendLine("</div>");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(html.ToString());
+        }
+    }
+}
---
+++ 02.Programming-Fundamentals-With-CSharp/08.TextProcessing-MoreExercise/TextProcessingMoreExercise/TreasureFinder/Finder.cs
@@ -0,0 +1,49 @@
+﻿namespace TreasureFinder
+{
+    #region Using
+
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    #endregion
+
+    internal class Finder
+    {
+        private static void Main(string[] args)
+        {
+            int[] key = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+            string input = Console.ReadLine();
+            while (input != "find")
+            {
+                StringBuilder decrypted = new StringBuilder();
+                int j = 0;
+
+                for (int i = 0; i < input?.Length; i++)
+                {
+                    decrypted.Append((char) (input[i] - key[j]));
+
+                    j++;
+                    if (j == key.Length)
+                    {
+                        j = 0;
+                    }
+                }
+
+                string message = decrypted.ToString();
+                int startIndex = message.IndexOf("&");
+                int endIndex = message.IndexOf("&", startIndex + 1);
+                string type = message.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+                startIndex = message.IndexOf("<");
+                endIndex = message.IndexOf(">", startIndex);
+                string coordinates = message.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
