cba1db398cd70a65f5b3cbd88e6378f57fe160d3
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2021-09-20 09:14:09 +0300

    Upload "Stacks ans Queues - Exercise" homework

---
+++ CSharp/03.CSharp-Advanced/02.Stacks and Queues - Exercise/StacksAndQueuesExercise/BalancedParentheses/Balance.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+
+            Stack<char> first = new Stack<char>();
+            Queue<char> second = new Queue<char>();
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (i < sequence.Length / 2)
+                {
+                    first.Push(sequence[i]);
+                }
+                else
+                {
+                    second.Enqueue(sequence[i]);
+                }
+            }
+
+            if (first.Count != second.Count)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                bool areBalanced = true;
+                while (first.Count > 0)
+                {
+                    char open = first.Pop();
+                    char close = second.Dequeue();
+                    areBalanced = CheckBalance(open, close);
+                    if (areBalanced == false)
+                    {
+                        break;
+                    }
+                }
+
+                if (areBalanced)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+
+        private static bool CheckBalance(char open, char close)
+        {
+            bool areBalanced = false;
+            if (open == '(' && close == ')')
+            {
+                areBalanced = true;
+            }
+            else if (open == '[' && close == ']')
+            {
+                areBalanced = true;
+            }
+            else if (open == '{' && close == '}')
+            {
+                areBalanced = true;
+            }
+
+            return areBalanced;
+        }
+    }
+}
---
+++ CSharp/03.CSharp-Advanced/02.Stacks and Queues - Exercise/StacksAndQueuesExercise/TruckTour/Tour.cs
@@ -0,0 +1,42 @@
+﻿namespace TruckTour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tour
+    {
+        private static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+            for (int i = 0; i < count; i++)
+            {
+                int[] data = Console.ReadLine()?.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                    .ToArray();
+                pumps.Enqueue(data);
+            }
+
+            int index = 0;
+            while (true)
+            {
+                int[] pumpData = pumps.Dequeue();
+                capacity += pumpData[0];
+                if (capacity < pumpData[1])
+                {
+                    capacity = 0;
+                    index++;
+                    pumps.Enqueue(pumpData);
+                }
+
+                if (pumps.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
