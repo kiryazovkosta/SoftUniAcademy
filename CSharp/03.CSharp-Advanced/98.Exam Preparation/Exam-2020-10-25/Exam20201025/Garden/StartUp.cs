97cd7c1b67a37d1608ea1d8d4d84cc0840a3e7df
kiryazovkosta <kosta.kiryazov@gmail.com>
2021-10-22 21:21:51 +0300

    Finish "C# Advanced Exam - 25 October 2020"

--- CSharp/03.CSharp-Advanced/98.Exam Preparation/Exam-2020-10-25/Exam20201025/Garden/StartUp.cs
+++ CSharp/03.CSharp-Advanced/98.Exam Preparation/Exam-2020-10-25/Exam20201025/Garden/StartUp.cs
@@ -21,7 +21,7 @@ namespace Garden
             string input = Console.ReadLine();
             while (input != "Bloom Bloom Plow")
             {
-                int[] positions = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                int[] positions = input.Split(" ").Select(int.Parse).ToArray();
                 int rowIndex = positions[0];
                 int colIndex = positions[1];
 
@@ -53,6 +53,11 @@ namespace Garden
                 input = Console.ReadLine();
             }
 
+            Print(garden);
+        }
+
+        private static void Print(int[,] garden)
+        {
             for (int rowIndex = 0; rowIndex < garden.GetLength(0); rowIndex++)
             {
                 for (int colIndex = 0; colIndex < garden.GetLength(1); colIndex++)
