e7f7a40b2ef44c00405eb6a2dcb06749a413a307
kiryazovkosta <kosta.kiryazov@gmail.com>
2021-10-22 21:08:05 +0300

    Upload "C# Advanced Retake Exam - 14 April 2021"

---
+++ CSharp/03.CSharp-Advanced/98.Exam Preparation/Exam-2021-04-14/Exam20210414/CocktailParty/Cocktail.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.MaxAlcoholLevel = maxAlcoholLevel;
+            this.Ingredients = new List<Ingredient>();
+        }
+
+        public List<Ingredient> Ingredients;
+        public string Name { get; set; }
+        public int  Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel => Ingredients.Sum(i => i.Alcohol);
+
+        public void Add(Ingredient ingredient)
+        {
+            if (!this.Ingredients.Any(i => i.Name == ingredient.Name)
+                && this.Capacity > this.Ingredients.Count
+                && this.MaxAlcoholLevel >= ingredient.Alcohol + CurrentAlcoholLevel)
+            {
+                this.Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient ingredient = this.Ingredients.FirstOrDefault(i => i.Name == name);
+            if (ingredient == null)
+            {
+                return false;
+            }
+
+            this.Ingredients.Remove(ingredient);
+            return true;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return this.Ingredients.FirstOrDefault(i => i.Name == name);
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return this.Ingredients.OrderByDescending(i => i.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {this.Name}-Current Alcohol Level: {this.CurrentAlcoholLevel}");
+            foreach (var ingredient in this.Ingredients)
+            {
+                sb.AppendLine(ingredient.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
