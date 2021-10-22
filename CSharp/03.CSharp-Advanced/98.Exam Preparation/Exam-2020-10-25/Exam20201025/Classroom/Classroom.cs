ac65a4c14fc436da65aa8bedde82275a02439e9b
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2021-10-22 16:48:47 +0300

    Upload "C# Advanced Exam - 25 October 2020"

---
+++ CSharp/03.CSharp-Advanced/98.Exam Preparation/Exam-2020-10-25/Exam20201025/Classroom/Classroom.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.Capacity > this.Count)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            
+            return "No seats in the classroom";
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return "Student not found";
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            var student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            return student;
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var enrolledStudents = students.Where(s => s.Subject == subject).ToList();
+            if (enrolledStudents.Count == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+            foreach (var student in students.Where(s => s.Subject == subject))
+            {
+                sb.AppendLine($"{student.FirstName} {student.LastName}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ CSharp/03.CSharp-Advanced/98.Exam Preparation/Exam-2020-10-25/Exam20201025/Scheduling/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace Scheduling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var tasks = new Stack<int>(input.Split(", ").Select(int.Parse));
+            input = Console.ReadLine();
+            var threads = new Queue<int>(input.Split(" ").Select(int.Parse));
+            input = Console.ReadLine();
+            int taskToKill = int.Parse(input);
+
+            int killerThread = 0;
+            while (tasks.Count > 0 && threads.Count > 0)
+            {
+                int task = tasks.Peek();
+                int thread = threads.Peek();
+
+                if (taskToKill == task)
+                {
+                    killerThread = thread;
+                    break;
+                }
+
+
+                if (thread >= task)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+
+            }
+
+            Console.WriteLine($"Thread with value {killerThread} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", threads));
+        }
+    }
+}
