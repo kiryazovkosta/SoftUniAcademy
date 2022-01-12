61ee0c553da1e3b318cddf417ab36570cfeaf2e8
kiryazovkosta <kosta.kiryazov@gmail.com>
2022-01-12 11:04:45 +0200

    Upload "Syntax, Functions and Statements - Lab" JS Homework

--- JavaScript/02.JS-Advanced/03.Syntax-Functions-Statements-Lab/src/circle-area.js
+++ JavaScript/02.JS-Advanced/03.Syntax-Functions-Statements-Lab/src/circle-area.js
@@ -1,3 +1,13 @@
-function area() {
-    
+function area(radius) {
+    let radiusType = typeof(radius);
+    if (radiusType === "number") {
+        let result = radius ** 2 * Math.PI;
+        console.log(result.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${radiusType}.`);
+    }
 }
+
+area(5);
+area('name');
+area(undefined);
