2bb1e68ffb3b60d83af7718b4b3c795731976cfa
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2022-08-04 10:19:00 +0300

    Upload "C# DB Advanced Retake Exam - 16 Dec 2021"

---
+++ CSharp/06.Entity Framework Core/98.Exam preparations/2021-12-16/Artillery/Artillery/DataProcessor/ExportDto/ExportGunDto.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Artillery.DataProcessor.ExportDto
+{
+    public class ExportGunDto
+    {
+        public string GunType { get; set; }
+
+        public int GunWeight { get; set; }
+
+        public double BarrelLength { get; set; }
+
+        public string Range { get; set; }
+    }
+}
---
+++ CSharp/06.Entity Framework Core/98.Exam preparations/2021-12-16/Artillery/Artillery/DataProcessor/ImportDto/ImportCountyIndexDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    public class ImportCountyIndexDto
+    {
+        [JsonProperty("Id")]
+        [Required]
+        public int Id { get; set; }
+
+    }
+}
